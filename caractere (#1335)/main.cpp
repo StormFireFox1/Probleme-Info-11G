5b5ee4653ee874c97dc609474ac72760841ec71d
StormFireFox1 <matei.gardus@gmail.com>
2017-09-21 17:48:24 +0300

    Rectificare sursa caractere (100p)

--- caractere (#1335)/main.cpp
+++ caractere (#1335)/main.cpp
@@ -10,12 +10,15 @@ int main()
     cin.getline(c, 256);
     cin.getline(s, 256);
     int l1 = strlen(c), l2 = strlen(s);
-    for(int j = 0; j < l2; j++)
+    for(int i = 0; i < l1; i++)
     {
-     	for(int i = 0; i < l1; i++)
+     	for(int j = 0; j < l2; j++)
         {
          	if(c[i] == s[j])
+            {
                 cout << s[j];
+                break;
+            }
         }
     }
     return 0;
