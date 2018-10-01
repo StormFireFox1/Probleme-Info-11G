92a86f94d64e18beb24df00efe7faa4a453bea39
StormFireFox1 <matei@gardus.eu>
2018-10-01 09:20:10 +0300

    Adaugare exercitiu 4

--- Clasa_12/atestat/4/main.cpp
+++ Clasa_12/atestat/4/main.cpp
@@ -1,50 +1,55 @@
 #include <fstream>
-#include <iostream>
+#include <cmath>
 
 using namespace std;
 
 ifstream fin("date.in");
+ofstream fout("date.out");
 
-int s1(int k) {
-    int max = -1;
-    while (k != 0) {
-        if (k % 10 > max) {
-            max = k % 10;
-        }
-        k /= 10;
-    }
-    return max;
+int s1(int k)
+{
+    int x = sqrt(k);
+    return x * x;
 }
 
-int s2(int k, int c) {
-    int newNumberInverse = 0;
-    while (k != 0) {
-        if (k % 10 == c) {
-            continue;
-        } else {
-            newNumberInverse = newNumberInverse * 10 + k % 10;
+void s2(int p, int q, int v[])
+{
+    bool ok = true;
+    while (ok)
+    {
+        for (int i = p; i < q; i++)
+        {
+            if (v[i] > v[i + 1])
+            {
+                int aux = v[i];
+                v[i] = v[i + 1];
+                v[i + 1] = aux;
+                ok = 1;
+            }
         }
-        k /= 10;
     }
-    int newNumber = 0;
-    while (newNumberInverse != 0) {
-        newNumber = newNumber * 10 + newNumberInverse % 10;
-        newNumberInverse /= 10;
-    }
-    return newNumber;
 }
 
-int main() {
+int main()
+{
     int x;
-    while (fin >> x) {
-        // code here
-        int out = 0;
-        while (x != 0) {
-            int c = s1(x);
-            out = out * 10 + c;
-            x = s2(x, c);
+    int i;
+    int j = 1;
+    int n, v[100];
+    fin >> n;
+    for (i = 1; i <= n; i++)
+    {
+        fin >> x;
+        if (x == s1(x))
+        {
+            v[j] = x;
+            j++;
         }
-        cout << out << " ";
+    }
+    s2(1, j - 1, v);
+    for (int i = 1; i < j; i++)
+    {
+        fout << v[i] << " ";
     }
     return 0;
 }
